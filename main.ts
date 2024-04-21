15bbae3a83058b541456d3924d10e3260d7d2155
AryanNaik <aryannaikofficial@gmail.com>
2024-04-21 17:11:02 +0530

    Implemented basic variable decalration using let and constants

--- main.ts
+++ main.ts
@@ -11,11 +11,11 @@ function repl(){
     const parser = new Parser();
     const env = new Environment();
 
-    env.declareVar("x",MK_NUMBER(100));
-    env.declareVar("true",MK_BOOL(true));
-    env.declareVar("false",MK_BOOL(false));
+    // env.declareVar("x",MK_NUMBER(100));
+    // env.declareVar("true",MK_BOOL(true));
+    // env.declareVar("false",MK_BOOL(false));
 
-    env.declareVar("null",MK_NULL());
+    // env.declareVar("null",MK_NULL());
     console.log("\nAryanLang v0.1");
     console.log("\nEnter exit to quit.");
 
